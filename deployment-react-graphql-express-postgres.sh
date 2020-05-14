f112073d7a6ce1d579d8e64643fe2074859776ec
Gerold Neuwirt <gerold.neuwirt@gmail.com>
2020-05-14 15:36:52 +0200

    Improved script for deployment

--- deployment-react-graphql-express-postgres.sh
+++ deployment-react-graphql-express-postgres.sh
@@ -1,10 +1,50 @@
 #!/bin/bash
-echo "Stop Docker..." && \
-if $(docker ps | grep -q -i up); then docker stop $(docker ps -q); fi && \
-echo "Remove Container..." && \
-if $(docker ps -a | grep -q -i Exited); then docker rm -f $(docker ps -a -q); fi && \
-echo "Remove Images..." && \
-if $(docker images | grep -q -i "\." ); then docker rmi -f $(docker images -q); fi && \
 
-echo "Start Docker Compose ..." && \
+# Let other container still running as long as they have different name
+echo "Stop Docker Container ..."
+# List of container names to be stopped
+Container="web-app api postgres webserver"
+for container_name in $Container; do
+    if [ "$(docker ps -q -f name=$container_name)" ]; then
+        if [ $(docker inspect -f '{{.State.Running}}' $container_name) = "true" ]; then
+            echo "Stop Container: "$container_name
+            docker stop $container_name
+        fi
+    fi
+done
+
+if [[ $(docker ps -a | grep -i Exited) ]]; then
+    echo "Remove Container..."
+    docker rm $(docker ps -a | grep -i Exited | grep -Eo '^[^ ]+')
+fi
+if [[ $(docker images -a | grep -i ago) ]]; then
+    echo "Remove Images..."
+    docker image prune -af
+fi
+if $(docker volume ls -qf dangling=true | grep "\."); then
+    echo "Clean up orphaned volumes"
+    docker volume rm $(docker volume ls -qf dangling=true)
+fi
+if $(docker network ls -q | grep "\."); then
+    echo "Clean up orphaned networks"
+    docker network rm $(docker network ls -q)
+fi
+
+echo "Start Docker Compose ..."
 docker-compose up -d
+
+Container="web-app api postgres webserver"
+for container_name in $Container; do
+    if [ "$(docker ps -q -f name=$container_name)" ]; then
+        if [ ! $(docker inspect -f '{{.State.Running}}' $container_name) = "true" ]; then
+            echo "Container not running: "$container_name
+            docker ps -a
+            exit
+        fi
+    fi
+done
+
+docker ps -a
+
+echo
+echo "** Deployment successful **"
